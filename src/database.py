87ce1f8918d6fcf85c91579b211a39ef615888d2
John Philip <developerphilo@gmail.com>
2023-10-01 20:43:36 +0300

    fix: get riid of MySQLl connect

--- src/database.py
+++ src/database.py
@@ -2,27 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
-import MySQLdb
-# Create an SQLAlchemy dialect for MySQLdb
-from sqlalchemy.dialects.mysql import mysqldb
 
-# Create a MySQLdb connection using MySQLdb.connect
-connection = MySQLdb.connect(
-    host=settings.DB_HOST,
-    user=settings.DB_USERNAME,
-    passwd=settings.DB_PASSWORD,
-    db=settings.DB_NAME,
-    autocommit=True,
-)
+MySQL_DATABASE_URL = settings.MySQL_DATABASE_URL
 
-
-engine = create_engine("mysql+mysqldb://", creator=lambda: connection, echo=True)
-
+engine = create_engine( MySQL_DATABASE_URL, echo=True)
+ 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 metadata = Base.metadata
-
 def get_db():
     db = SessionLocal()
     try:
